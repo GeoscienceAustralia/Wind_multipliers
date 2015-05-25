1090e29401aa0a0771a7726053399e72a0574f13
u89076 <tyang@rhe-compute1.ga.gov.au>
2015-05-25 14:43:50 +1000

    update test_clip_dataset.py

--- tests/test_clip_dataset.py
+++ tests/test_clip_dataset.py
@@ -1,8 +1,9 @@
 """
- Title: test_combine.py 
+ Title: test_clip_dataset.py 
  Author: Tina Yang, tina.yang@ga.gov.au 
  CreationDate: 2014-06-02
- Description: Unit testing module for combine function in shield_mult.py 
+ Description: Unit testing module for clip_dataset and reproject_dataset
+ function in all_multipliers.py 
  Version: $Rev$ 
  $Id$
 """
@@ -20,29 +21,36 @@ from os.path import exists
 
 class TestClip(unittest.TestCase):
 
-    def setUp(self):        
-               
-        self.test_rasterfile = "test_raster_dem.img" 
-        self.expect_result = 'test_raster_expect_clip_dem.img'
+    def setUp(self):
         
-        self.extent = [142.94, -22.97, 143.96, -23.98]
+        self.cmd_folder = os.path.realpath(os.path.abspath(
+                                    os.path.split(getfile(currentframe()))[0]))         
         
-        self.source_ds = "test_raster_lc.img"
-        self.match_ds = "test_raster_clip_dem.img"
-        
-        cmd_folder = os.path.realpath(os.path.abspath(os.path.split(getfile(currentframe()))[0]))         
-        
-        parent = os.path.abspath(os.path.join(cmd_folder, os.pardir))
+        parent = os.path.abspath(os.path.join(self.cmd_folder, os.pardir))
                 
         if parent not in sys.path:
             sys.path.insert(0, parent)
             
+       
+        
+#        print self.test_rasterfile
+#        print self.source_ds
+        
            
-           
-    def test_clip(self):          
+    def test_clip(self):  
+
+        dem = os.path.join(self.cmd_folder, "test_raster_dem.img") 
+        dem_expect_result = os.path.join(self.cmd_folder,
+                                          'test_raster_expect_clip_dem.img')
+        
+        extent = [142.94, -22.97, 143.96, -23.98]
+        
+        lc = os.path.join(self.cmd_folder, "test_raster_lc.img")
+        
+        
         from all_multipliers import Multipliers
         
-        mp = Multipliers(self.source_ds, self.test_rasterfile)
+        mp = Multipliers(lc, dem)
         
         extent = [143.2, -23.2, 143.4, -23.4] 
         
@@ -57,7 +65,7 @@ class TestClip(unittest.TestCase):
 #        script_band = script_dataset.GetRasterBand(1)
 #        script_data = script_band.ReadAsArray()
         
-        expect_dataset = gdal.Open(self.expect_result)
+        expect_dataset = gdal.Open(dem_expect_result)
 #        expect_band = expect_dataset.GetRasterBand(1)
 #        expect_data = expect_band.ReadAsArray()
         
@@ -74,17 +82,21 @@ class TestClip(unittest.TestCase):
         
         
     def test_reproject(self): 
+        
+        lc = os.path.join(self.cmd_folder, "test_raster_lc.img")        
+        match_ds = os.path.join(self.cmd_folder, "test_raster_clip_dem.img")
+        
         from all_multipliers import reproject_dataset
         
         dst_file = 'test_raster_dem_lc.img'        
         
-        reproject_dataset(self.source_ds, self.match_ds, dst_file) 
+        reproject_dataset(lc, match_ds, dst_file) 
         
         assert exists(dst_file)
         
         dst_dataset = gdal.Open(dst_file)
         
-        match_dataset = gdal.Open(self.match_ds)
+        match_dataset = gdal.Open(match_ds)
         
         # Check geographic transform:
         assert_almost_equal(dst_dataset.GetGeoTransform(),
