27f69cbfb20027a46ed4604ab4792410ad8f5a8b
Claire Krause <claire.krause@ga.gov.au>
2017-05-23 15:45:07 +1000

    Terrain classification now based on config, not hardcoded (#5)

--- utilities/value_lookup.py
+++ utilities/value_lookup.py
@@ -5,41 +5,6 @@
 MULTIPLIER_TYPE = ['terrain', 'shielding']
 
 
-TERRAIN_CLASS_DESC = dict([(1, 'City Buildings'),
-                           (2, 'Dense Forest'),
-                           (3, 'High Density Metro'),
-                           (4, 'Centre of small towns'),
-                           (5, 'suburban buildings'),
-                           (6, 'Long grass with few trees'),
-                           (7, 'Crops'),
-                           (8, 'Uncut Grass'),
-                           (9, 'Airport runways'),
-                           (10, 'Cut grass'),
-                           (11, 'Barren/Mining/desert(stones)'),
-                           (12, 'Water'),
-                           (13, 'Roads'),
-                           (14, 'orched/open forest'),
-                           (15, 'Mudflats/salt evaporators/sandy beaches')])
-
-
-# Mz initial value at 10 m
-MZ_INIT = dict([(1, 750),
-                (2, 774),
-                (3, 782),
-                (4, 806),
-                (5, 830),
-                (6, 919),
-                (7, 949),
-                (8, 1000),
-                (9, 1000),
-                (10, 1048),
-                (11, 1063),
-                (12, 1000),
-                (13, 1063),
-                (14, 898),
-                (15, 1084)])
-
-
 MS_INIT = dict([(1, 90),
                 (2, 100),
                 (3, 88),
