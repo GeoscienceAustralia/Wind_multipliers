15165b2af0d4e7897d204783cef2561bfbbbfb47
wcarthur <wcarthur@gmail.com>
2020-07-24 16:36:44 +1000

    Data path is now a command line arg
    Created dataset dimensions are set by the input dataset dimensions, not fixed
    
    Handles the case of an empty input file, but not in an ideal way (replace missing file with values of 1)

--- utilities/convert.py
+++ utilities/convert.py
@@ -1,6 +1,7 @@
 from functools import reduce
 from os import listdir
-
+from os.path import join as pjoin
+import logging as log
 from osgeo import gdal, osr
 from osgeo.gdal_array import BandReadAsArray
 import numpy as np
@@ -16,11 +17,12 @@ PROJECTION = SRS.ExportToWkt()
 
 
 class Converter:
-    def __init__(self, tiles):
+    def __init__(self, tiles, datapath):
         self.tiles = tiles
         self.directions = ['e', 'n', 'ne', 'nw', 's', 'se', 'sw', 'w']
         self.types = {'shielding': 'Ms', 'terrain': 'Mz', 'topographic': 'Mt'}
         self.tile_files = []
+        self.path = datapath
 
     def process_tile(self, tile):
         '''Convert NetCDF tiles into GeoTIFF tile
@@ -31,7 +33,15 @@ class Converter:
 
         # Create the raster in-memory to allow manipulating the bands
         driver = gdal.GetDriverByName('MEM')
-        out_ds = driver.Create('', 3600, 3600, 1, gdal.GDT_Float32)
+
+        # Get/set dimensions for the output bands:
+        tmpds = gdal.Open(pjoin(self.path, 'shielding/{0}_ms_n.nc'.format(tile)))
+        nx = tmpds.RasterXSize
+        ny = tmpds.RasterYSize
+
+        out_ds = driver.Create('', nx, ny, 1, gdal.GDT_Float32)
+
+        del tmpds
 
         # Add a band for each direction
         for _ in range(1, len(self.directions)):
@@ -46,8 +56,20 @@ class Converter:
 
             # Read the wind multipliers for the current direction
             for folder, unit in self.types.items():
-                ds = gdal.Open('/g/data/fj6/multipliers/{0}/{1}_{2}_{3}.nc'.format(folder, tile, unit.lower(), direction))
+                filename = pjoin(self.path, '{0}/{1}_{2}_{3}.nc'.format(folder, tile, unit.lower(), direction))
+                log.info(f"Opening {filename}")
+                try:
+                    ds = gdal.Open(filename)
+                except RuntimeError:
+                    log.error(f"Cannot open {filename}")
+                    log.error("Replacing all data with ones")
+                    ds = driver.Create('', nx, ny, 1, gdal.GDT_Float32)
+                    ds.SetGeoTransform(transform)
+                    band = ds.GetRasterBand(1)
+                    band.WriteArray(np.ones((nx, ny)))
+                
                 bands.append(ma.masked_values(BandReadAsArray(ds.GetRasterBand(1)), -9999))
+
                 if first:
                     transform = ds.GetGeoTransform()
                     first = False
@@ -57,7 +79,11 @@ class Converter:
 
             # Write the pre-multiplied value to a band
             band = out_ds.GetRasterBand(band_index)
-            band.WriteArray(multiplied.data)
+            try:
+                band.WriteArray(multiplied.data)
+            except ValueError:
+                log.error("Mismatch between output file and data array")
+                log.error(multiplied.data.shape)
             band.SetDescription(direction)
             band.ComputeStatistics(False)
             band.SetNoDataValue(-9999)
@@ -68,7 +94,8 @@ class Converter:
         out_ds.SetProjection(PROJECTION)
         out_ds.SetGeoTransform(transform)
 
-        filename = '/g/data/fj6/multipliers/{0}/{1}.nc'.format('M3', tile)
+        filename = pjoin(self.path, '{0}/{1}.tif'.format('M3', tile))
+        log.info(f"Saving data to {filename}")
         # Write out a GeoTIFF
         tif = gdal.Translate(filename, out_ds,
                              options=gdal.TranslateOptions(gdal.ParseCommandLine("-co COMPRESS=LZW -co TILED=YES")))
--- utilities/mpi_runner.py
+++ utilities/mpi_runner.py
@@ -1,15 +1,24 @@
+import argparse
+
 from mpi4py import MPI
 import os
 from convert import Converter
 import functools
 import operator
 from osgeo import gdal
+import logging as log
 
 # MPI wrapper to parallelize tile conversion on NCI
 comm = MPI.COMM_WORLD
 
+p = argparse.ArgumentParser()
+p.add_argument('-p', '--path', help="Base path of multiplier data to convert")
+
+args = p.parse_args()
+datapath = args.path
+
 if comm.rank == 0:
-    tiles = list({file.split('_')[0] for file in os.listdir('/g/data/fj6/multipliers/shielding')})
+    tiles = list({file.split('_')[0] for file in os.listdir(os.path.join(datapath, 'shielding/'))})
     tiles = [tiles[i::comm.size] for i in range(comm.size)]
 else:
     tiles = None
@@ -17,13 +26,14 @@ else:
 tiles = comm.scatter(tiles, root=0)
 
 results = []
-converter = Converter(tiles)
+converter = Converter(tiles, datapath)
 results = converter.run()
 
 results = comm.gather(results, root=0)
 
+log.info("Creating virtual raster table")
 if comm.rank == 0:
     tile_files = functools.reduce(operator.iconcat, results, [])
 
     if len(tile_files) > 0:
-        gdal.BuildVRT('/g/data/w85/s3-multipliers/wind-multipliers.vrt', tile_files)
+        gdal.BuildVRT(os.path.join(datapath, 'wind-multipliers.vrt'), tile_files)
